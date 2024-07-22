dcaeaa6026e0b34d39be8a4c1ff3851616c87f50
Tanbir Singh Banipal <34286253+realTB@users.noreply.github.com>
2024-07-22 16:11:46 -0400

    Update Chatroom SDK

--- room_sdk.py
+++ room_sdk.py
@@ -1,16 +1,19 @@
 import requests
 from fastapi import HTTPException
 
-parseschema = 'http://'
-parsehost = '127.0.0.1'
-parseport = '8003'
-parseapiurl = '/api'
+chatroomschema = 'http://'
+chatroomhost = '127.0.0.1'
+chatroomport = '8003'
+chatroomapiurl = '/api'
 
-parseserviceurl = parseschema + parsehost + ':' + parseport + parseapiurl
+chatroomserviceurl = chatroomschema + chatroomhost + ':' + chatroomport + chatroomapiurl
 def get_userInRoom(chatroom_uuid, cookie):
-    response = requests.get(url = parseserviceurl + '/userinroom', params={'chatroom_uuid': chatroom_uuid, 'cookie': cookie})
+    response = requests.get(url = chatroomserviceurl + '/userinroom', params={'chatroom_uuid': chatroom_uuid, 'cookie': cookie})
     if response.status_code != 200:
         raise HTTPException(status_code=400, detail="Unexpected response from Chatroom Service")
     if not response.json()['detail']:
         raise HTTPException(status_code=401, detail="User not in chatroom")
     return 
+
+def post_message(chatroom_uuid, message, cookie):
+    return requests.post(url = chatroomserviceurl + '/message',  params={'chatroom_uuid': chatroom_uuid, 'cookie': cookie, 'message': message})
