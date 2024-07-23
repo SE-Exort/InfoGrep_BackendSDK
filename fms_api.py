36e5d8665a153ffeffabac14a34d9c24ddf3c5f0
Tanbir Singh Banipal <34286253+realTB@users.noreply.github.com>
2024-07-22 21:40:28 -0400

    Don't send the user_uuid to the FMS Service

--- fms_api.py
+++ fms_api.py
@@ -6,15 +6,15 @@ fmsport = '8002'
 fmsapiurl = '/api'
 
 fmsserviceurl = fmsschema + fmshost + ':' + fmsport + fmsapiurl
-def fms_getFileList(user_uuid, chatroom_uuid, cookie):
-    return requests.get(url = fmsserviceurl + '/filelist', params={'user_uuid': user_uuid, 'chatroom_uuid': chatroom_uuid, 'cookie': cookie})
+def fms_getFileList(chatroom_uuid, cookie):
+    return requests.get(url = fmsserviceurl + '/filelist', params={'chatroom_uuid': chatroom_uuid, 'cookie': cookie})
 
-def fms_getFile(user_uuid, chatroom_uuid, file_uuid, cookie):
-    return requests.get(url = fmsserviceurl + '/file', params={'user_uuid': user_uuid, 'chatroom_uuid': chatroom_uuid, 'file_uuid': file_uuid, 'cookie': cookie})
+def fms_getFile(chatroom_uuid, file_uuid, cookie):
+    return requests.get(url = fmsserviceurl + '/file', params={'chatroom_uuid': chatroom_uuid, 'file_uuid': file_uuid, 'cookie': cookie})
 
-def fms_postFile(user_uuid, chatroom_uuid, uploadedfile, cookie):
+def fms_postFile(chatroom_uuid, uploadedfile, cookie):
     file = {'uploadedfile': open(uploadedfile, 'rb')}
-    return requests.post(url = fmsserviceurl + '/file', params={'user_uuid': user_uuid, 'chatroom_uuid': chatroom_uuid, 'cookie': cookie}, files=file)
+    return requests.post(url = fmsserviceurl + '/file', params={'chatroom_uuid': chatroom_uuid, 'cookie': cookie}, files=file)
 
-def fms_deleteFile(user_uuid, chatroom_uuid, file_uuid, cookie):
-    return requests.delete(url = fmsserviceurl + '/file', params={'user_uuid': user_uuid, 'chatroom_uuid': chatroom_uuid, 'file_uuid': file_uuid, 'cookie': cookie})
+def fms_deleteFile(chatroom_uuid, file_uuid, cookie):
+    return requests.delete(url = fmsserviceurl + '/file', params={'chatroom_uuid': chatroom_uuid, 'file_uuid': file_uuid, 'cookie': cookie})
