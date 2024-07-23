a524e880716b9aa8d89ffd3ec1fd2775d8ff7b5b
Tanbir Singh Banipal <34286253+realTB@users.noreply.github.com>
2024-07-22 21:16:10 -0400

    Don't Send User_UUIDs to the Parsing Service

--- parse_api.py
+++ parse_api.py
@@ -6,11 +6,11 @@ parseport = '8001'
 parseapiurl = '/api'
 
 parseserviceurl = parseschema + parsehost + ':' + parseport + parseapiurl
-def parse_postStartParsing(user_uuid, chatroom_uuid, file_uuid, filetype, cookie):
-    return requests.post(url = parseserviceurl + '/start_parsing', params={'user_uuid': user_uuid, 'chatroom_uuid': chatroom_uuid, 'file_uuid': file_uuid, 'filetype': filetype, 'cookie': cookie})
+def parse_postStartParsing(chatroom_uuid, file_uuid, filetype, cookie):
+    return requests.post(url = parseserviceurl + '/start_parsing', params={'chatroom_uuid': chatroom_uuid, 'file_uuid': file_uuid, 'filetype': filetype, 'cookie': cookie})
 
-def parse_postCancelParsing(user_uuid, chatroom_uuid, file_uuid, cookie):
-    return requests.post(url = parseserviceurl + '/cancel_parsing', params={'user_uuid': user_uuid, 'chatroom_uuid': chatroom_uuid, 'file_uuid': file_uuid, 'cookie': cookie})
+def parse_postCancelParsing(chatroom_uuid, file_uuid, cookie):
+    return requests.post(url = parseserviceurl + '/cancel_parsing', params={'chatroom_uuid': chatroom_uuid, 'file_uuid': file_uuid, 'cookie': cookie})
 
-def parse_getParsingStatus(user_uuid, chatroom_uuid, file_uuid, cookie):
-    return requests.get(url = parseserviceurl + '/parsing_status', params={'user_uuid': user_uuid, 'chatroom_uuid': chatroom_uuid, 'file_uuid': file_uuid, 'cookie': cookie})
+def parse_getParsingStatus(chatroom_uuid, file_uuid, cookie):
+    return requests.get(url = parseserviceurl + '/parsing_status', params={'chatroom_uuid': chatroom_uuid, 'file_uuid': file_uuid, 'cookie': cookie})
