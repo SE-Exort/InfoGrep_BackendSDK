b0e07ea7d3a97cc920b3f2a315320f210ecc722a
TyroneHe-0926 <tyrone99926@gmail.com>
2025-01-21 03:50:30 -0500

    fix: key not exist error

--- util.py
+++ util.py
@@ -2,7 +2,7 @@ def header_cleanup(headers):
     if not isinstance(headers, dict):
         headers = dict(headers)
     
-    headers.pop("content-type")
-    headers.pop("content-length")
+    headers.pop("content-type", None)
+    headers.pop("content-length", None)
 
     return headers
