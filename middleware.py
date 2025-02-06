92aa7cefa82b507fee846c459fe0fc555daf9527
TyroneHe-0926 <tyrone99926@gmail.com>
2025-02-05 20:00:31 -0500

    fix: undo status code str cast

--- middleware.py
+++ middleware.py
@@ -24,15 +24,14 @@ class LoggingMiddleware(BaseHTTPMiddleware):
         self.logger.info(msg=f"Request sent by {request.client.host} to {request.url} started")
         
         response = await call_next(request)
-        status_code = str(response.status_code)
-        if status_code.startswith('4') or status_code.startswith('5'):
+        if response.status_code >= 400:
             self.logger.error(
                 msg=f"ERROR: Request sent by {request.client.host} to {request.url} failed",
                 extra={
                     "status_code": status_code,
                     "content": response.body()
                 })
-        elif status_code == "200":
+        elif response.status_code == 200:
             self.logger.info(
                 msg=f"Request sent by {request.client.host} to {request.url} completed",
                 extra={
