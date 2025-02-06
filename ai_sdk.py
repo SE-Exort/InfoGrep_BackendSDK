e381a463ab10f74a3a07bb55e7fccff41eb06196
TyroneHe-0926 <tyrone99926@gmail.com>
2025-02-06 01:42:51 -0500

    chore: adding parsing function calls to the ai sdk

--- ai_sdk.py
+++ ai_sdk.py
@@ -6,3 +6,15 @@ aiserviceurl = service_schema + ai_host + ai_apiurl
 
 def get_Response(chatroom_uuid, message, cookie, model, headers={}):
     requests.get(url = aiserviceurl + '/system_response', params={'chatroom_uuid': chatroom_uuid, 'message': message, 'cookie': cookie, 'model': model}, headers=header_cleanup(headers))
+
+def parse_postStartParsing(chatroom_uuid, file_uuid, filetype, cookie, headers={}):
+    return requests.post(url = aiserviceurl + '/start_parsing', params={'chatroom_uuid': chatroom_uuid, 'file_uuid': file_uuid, 'filetype': filetype, 'cookie': cookie}, headers=header_cleanup(headers))
+
+def parse_postCancelParsing(chatroom_uuid, file_uuid, cookie, headers={}):
+    return requests.post(url = aiserviceurl + '/cancel_parsing', params={'chatroom_uuid': chatroom_uuid, 'file_uuid': file_uuid, 'cookie': cookie}, headers=header_cleanup(headers))
+
+def parse_getParsingStatus(chatroom_uuid, file_uuid, cookie, headers={}):
+    return requests.get(url = aiserviceurl + '/parsing_status', params={'chatroom_uuid': chatroom_uuid, 'file_uuid': file_uuid, 'cookie': cookie}, headers=header_cleanup(headers))
+
+def get_supported_filetypes(headers={}):
+    return requests.get(url = aiserviceurl + '/file_types', headers=header_cleanup(headers))
